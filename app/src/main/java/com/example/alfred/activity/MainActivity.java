8bb0fe157fd41e237c6f18cd121a6cc5ddda5f3a
Christy Hauschild Schott <christyhauschild@gmail.com>
2021-03-30 21:01:49 -0300

    feat: update buttons actions

--- app/src/main/java/com/example/alfred/activity/MainActivity.java
+++ app/src/main/java/com/example/alfred/activity/MainActivity.java
@@ -2,15 +2,58 @@ package com.example.alfred.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.alfred.R;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextInputLayout txMainEmail, txMainPassword;
+    Button btnMainSignIn, btnDoNotHaveAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txMainEmail = findViewById(R.id.txMainEmail);
+        txMainPassword = findViewById(R.id.txMainPassword);
+
+        btnMainSignIn = findViewById(R.id.btnMainSignIn);
+        btnDoNotHaveAccount = findViewById(R.id.btnDoNotHaveAccount);
+
+        btnMainSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (txMainEmail.getEditText().getText().toString().equals("")){
+                    txMainEmail.setError("Informe o e-mail");
+                    txMainEmail.requestFocus();
+                } else if (txMainPassword.getEditText().getText().toString().equals("")){
+                    txMainPassword.setError("Informe a senha");
+                    txMainPassword.requestFocus();
+                } else {
+                    String email = txMainEmail.getEditText().getText().toString();
+                    String password = txMainPassword.getEditText().getText().toString();
+
+                    if (!email.equals("") && !password.equals("")){
+                        Intent it = new Intent(MainActivity.this, MenuActivity.class);
+                        it.putExtra("email", email);
+                        startActivity(it);
+                    }
+                }
+            }
+        });
+
+        btnDoNotHaveAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(it);
+            }
+        });
     }
 }
